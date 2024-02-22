b6b6140d3893db10956b91658954de15451f3f11
MeganKR <megankaye2018@gmail.com>
2024-02-22 11:00:40 -0600

    setup userRoutes and thoughtRoutes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -59,4 +59,42 @@ module.exports = {
           res.status(500).json(err);
         }
       },
-}
+      async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.body.friendId } },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res
+                  .status(404)
+                  .json({ message: 'No user found with that ID' });
+              }
+
+              res.json(student);
+            } catch (err) {
+              res.status(500).json(err);
+            }
+        },
+        async removeFriend(req, res) {
+            try {
+              const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: { friendId: req.params.friendIdId } } },
+                { runValidators: true, new: true }
+              );
+        
+              if (!user) {
+                return res
+                  .status(404)
+                  .json({ message: 'No student found with that ID :(' });
+              }
+        
+              res.json(user);
+            } catch (err) {
+              res.status(500).json(err);
+            }
+          },
+      };
